0698f0957dfd69cad928836e9df17fbebb1cbed9
墨羽翎玖 <1597867839@qq.com>
2022-01-22 17:33:27 +0800

    初始化项目

--- entity/group.py
+++
@@ -1,7 +0,0 @@
-class Group:
-    def __init__(
-            self,
-            group_id: int,
-            group_name: str
-    ):
-        pass
--- util/common_kit.py
+++
@@ -1,6 +0,0 @@
-"""基础工具类"""
-import time
-
-
-def output_time(date) -> str:
-    return time.strftime("%Y-%m-%d %H:%M:%S", date)
